417b9b63d8f59417970239f4fd96419fb238ca74
Marcos Santos <89700349+marcos-nsantos@users.noreply.github.com>
2023-03-31 20:32:56 -0500

    feat: get port from os environment and run server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,8 +1,25 @@
 package main
 
-import "github.com/marcos-nsantos/adopet-backend/internal/database"
+import (
+	"log"
+	"os"
+
+	"github.com/marcos-nsantos/adopet-backend/internal/database"
+	"github.com/marcos-nsantos/adopet-backend/internal/router"
+)
 
 func main() {
 	database.Init()
 	database.Migrate()
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	r := router.SetupRoutes()
+	err := r.Run(":" + port)
+	if err != nil {
+		log.Fatalln("Error running server: ", err)
+	}
 }
