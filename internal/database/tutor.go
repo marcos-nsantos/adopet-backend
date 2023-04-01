f3257426dcfbb48d45e92263c4fb5ae69de0bbfc
Marcos Santos <89700349+marcos-nsantos@users.noreply.github.com>
2023-03-31 19:57:16 -0500

    feat: add function to get a tutor by id

--- internal/database/tutor.go
+++ internal/database/tutor.go
@@ -5,3 +5,9 @@ import "github.com/marcos-nsantos/adopet-backend/internal/entity"
 func CreateTutor(tutor *entity.Tutor) error {
 	return DB.Create(tutor).Error
 }
+
+func GetTutorByID(id uint64) (entity.Tutor, error) {
+	var tutor entity.Tutor
+	result := DB.Omit("password", "deleted_at").First(&tutor, id)
+	return tutor, result.Error
+}
--- internal/database/tutor_test.go
+++ internal/database/tutor_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/marcos-nsantos/adopet-backend/internal/mock"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
@@ -23,6 +24,38 @@ func TestCreateTutor(t *testing.T) {
 
 	t.Run("should not create a tutor when email is already in use", func(t *testing.T) {
 		err := CreateTutor(&tutor)
+		require.Error(t, err)
+	})
+}
+
+func TestGetTutorByID(t *testing.T) {
+	Init()
+	Migrate()
+
+	t.Cleanup(func() {
+		DropTables()
+	})
+
+	tutor := mock.Tutors()[0]
+	err := CreateTutor(&tutor)
+	require.NoError(t, err)
+
+	t.Run("should get a tutor by id", func(t *testing.T) {
+		tutorFound, err := GetTutorByID(tutor.ID)
+		require.NoError(t, err)
+		assert.Equal(t, tutor.ID, tutorFound.ID)
+		assert.Equal(t, tutor.Name, tutorFound.Name)
+		assert.Equal(t, tutor.Email, tutorFound.Email)
+		assert.Empty(t, tutorFound.Password)
+		assert.Equal(t, tutor.Phone, tutorFound.Phone)
+		assert.Equal(t, tutor.Photo, tutorFound.Photo)
+		assert.Equal(t, tutor.City, tutorFound.City)
+		assert.Equal(t, tutor.About, tutorFound.About)
+		assert.Empty(t, tutorFound.DeletedAt)
+	})
+
+	t.Run("should not get a tutor by id when tutor does not exist", func(t *testing.T) {
+		_, err := GetTutorByID(0)
 		require.Error(t, err)
 	})
 }
