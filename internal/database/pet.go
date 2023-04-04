e4497157c81b84303cd262c1e36f96bbd30b9c18
Marcos Santos <89700349+marcos-nsantos@users.noreply.github.com>
2023-04-03 19:18:02 -0500

    feat: add function to get pet by from database

--- internal/database/pet.go
+++ internal/database/pet.go
@@ -6,3 +6,9 @@ func CreatePet(pet entity.Pet) (entity.Pet, error) {
 	result := DB.Create(&pet)
 	return pet, result.Error
 }
+
+func GetPetByID(id uint64) (entity.Pet, error) {
+	var pet entity.Pet
+	result := DB.Select("id", "name", "description", "is_adopt", "age", "photo", "uf", "city").First(&pet, id)
+	return pet, result.Error
+}
--- internal/database/pet_test.go
+++ internal/database/pet_test.go
@@ -30,3 +30,40 @@ func TestCreatePet(t *testing.T) {
 		assert.Equal(t, pet.City, result.City)
 	})
 }
+
+func TestGetPetByID(t *testing.T) {
+	Init()
+	Migrate()
+
+	t.Cleanup(func() {
+		DropTables()
+	})
+
+	pet := mock.Pet()[0]
+	result, err := CreatePet(pet)
+	require.NoError(t, err)
+
+	t.Run("should get a pet by id", func(t *testing.T) {
+		result, err := GetPetByID(result.ID)
+		require.NoError(t, err)
+
+		assert.Equal(t, pet.Name, result.Name)
+		assert.Equal(t, pet.Description, result.Description)
+		assert.Equal(t, pet.IsAdopt, result.IsAdopt)
+		assert.Equal(t, pet.Age, result.Age)
+		assert.Equal(t, pet.Photo, result.Photo)
+		assert.Equal(t, pet.UF, result.UF)
+		assert.Equal(t, pet.City, result.City)
+	})
+
+	t.Run("should return error when pet is not found", func(t *testing.T) {
+		_, err := GetPetByID(0)
+		require.Error(t, err)
+	})
+
+	t.Run("should return error when pet is deleted", func(t *testing.T) {
+		DB.Delete(&result)
+		_, err := GetPetByID(result.ID)
+		require.Error(t, err)
+	})
+}
