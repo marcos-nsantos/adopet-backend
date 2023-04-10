436292503a0aebd96e012954b10834be16bd7659
Marcos Santos <89700349+marcos-nsantos@users.noreply.github.com>
2023-04-10 14:19:11 -0500

    feat: add function to validate token

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -17,3 +17,25 @@ func GenerateToken(userID uint64, userType string) (string, error) {
 	})
 	return token.SignedString([]byte(secretKey))
 }
+
+func validateToken(tokenString string) (bool, error) {
+	token, err := parseToken(tokenString)
+	if err != nil {
+		return false, err
+	}
+
+	if _, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		return true, nil
+	}
+
+	return false, nil
+}
+
+func parseToken(tokenString string) (*jwt.Token, error) {
+	return jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, nil
+		}
+		return []byte(secretKey), nil
+	})
+}
