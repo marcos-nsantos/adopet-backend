c9781e95c3fde74bbf000db0fbe1c8e172b01689
Marcos Santos <89700349+marcos-nsantos@users.noreply.github.com>
2023-04-10 16:14:33 -0500

    feat: add function to compare password with hash

--- internal/password/password_test.go
+++ internal/password/password_test.go
@@ -13,3 +13,19 @@ func TestHashPassword(t *testing.T) {
 	require.NoError(t, err)
 	assert.NotEqual(t, password, hash)
 }
+
+func TestCompare(t *testing.T) {
+	password := "secretPassword123"
+	hash, err := Hash(password)
+	require.NoError(t, err)
+
+	t.Run("should return nil when password is correct", func(t *testing.T) {
+		err = Compare(hash, password)
+		assert.NoError(t, err)
+	})
+
+	t.Run("should return error when password is incorrect", func(t *testing.T) {
+		err = Compare(hash, "wrongPassword")
+		assert.Error(t, err)
+	})
+}
